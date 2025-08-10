25ef85958fa9dc2191e1229c5b76ea632e12b85e
Angel Sierra <ancilli0520@gmail.com>
2025-08-10 17:55:36 -0500

    Se agrega la funcionalidad para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//VARIABLES NECESARIAS PARA EL DESARROLLO DEL EJERCICIO
+let amigo = '';
+
+// CREAMOS EL ARRAY DONDE ALMACENAREMOS LOS NOMBRES INGRESADOS POR EL USUARIO  
+let amigos = [];
+
+// CREAMOS UNA FUNCION PARA IMPIAR EL INPUT 
+function limpiar() {
+    document.querySelector('#amigo').value = '';
+}
+
+// CREAMOS UNA FUNCION QUE NOS PERMITA INGRESAR A NUESTRO ARRAY LOS AMIGOS INGRESADOS POR EL USUARIO
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo == '') {
+        alert('Por favor, inserte un nombre.');
+    }else{  
+        amigos.push(amigo);
+        limpiar();
+        console.log(amigos);
+    }
+}
